0244ad25add65ed866236f1619e1bf693c6ebb6f
Ganesh Koilada <itsmeganeshcse@gmail.com>
2019-12-03 19:14:05 +0530

    Side Menu updated with items

--- components/SideMenuC/SideMenu.js
+++ components/SideMenuC/SideMenu.js
@@ -2,8 +2,9 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import styles from './SideMenu.style';
 import {NavigationActions} from 'react-navigation';
-import {ScrollView, Text, View} from 'react-native';
+import {ScrollView, Text, View,Alert} from 'react-native';
 import { Subheading } from 'react-native-paper';
+import { Icon,ListItem } from 'react-native-elements'
 
 
 //latest imports
@@ -20,21 +21,66 @@ class SideMenu extends Component {
   }
 
   render () {
+    const TopNavItems = [
+        {
+          name: 'Home',
+          avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        },
+        {
+          name: 'Notifications',
+          avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        },
+        {
+            name: 'Settings',
+            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        },
+        {
+            name: 'Help',
+            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        },
+        {
+            name: 'About',
+            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        },
+      ]
+
+      const BottomNavItems = [
+        {
+          name: 'Logout',
+          avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        },
+    ]
     return (
       <View style={{flex:1}}>
           <View style={{flex:2.5,backgroundColor:'green'}}>
 
           </View>
-          <View style={{flex:6,backgroundColor:'yellow'}}>
-               <View style={{flex:1}}><Text>Home</Text></View>
-               <View style={{flex:1}}><Text>Reports</Text></View>
-               <View style={{flex:1}}><Text>Notifications</Text></View>
-               <View style={{flex:1}}><Text>Settings</Text></View>
-               <View style={{flex:1}}><Text>About</Text></View>
-               <View style={{flex:1}}><Text>Help</Text></View>
+          <View style={{flex:6,backgroundColor:'white'}}>
+          {
+                TopNavItems.map((l, i) => (
+                <ListItem
+                    key={i}
+                    leftAvatar={{ source: { uri: l.avatar_url } }}
+                    title={l.name}
+                    subtitle={l.subtitle}
+                    // bottomDivider
+                />
+                ))
+            }
           </View>
-        <View style={{flex:1.5,backgroundColor:'red'}}>
-        <View style={{flex:1}}><Text>Logout</Text></View>
+        <View style={{flex:1.5,backgroundColor:'white'}}>
+        {
+            BottomNavItems.map((l, i) => (
+                <ListItem
+                    key={i}
+                    leftAvatar={{ source: { uri: l.avatar_url } }}
+                    title={l.name}
+                    subtitle={l.subtitle}
+                    // bottomDivider
+                    topDivider
+                />
+                ))
+            }
         </View>
       </View>
     );
